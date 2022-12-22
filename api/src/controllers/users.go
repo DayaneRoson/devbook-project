3323a440ae3e6c1b45b80bd6fab00a6f92d7fb83
Dayane Roson <dayane.roson@gmail.com>
2022-12-22 17:54:19 -0300

    package repositories - create user route working

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -1,28 +1,57 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
-//CreateUser creates an user
+// CreateUser creates an user
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating user"))
+	requestBody, error := io.ReadAll(r.Body)
+	if error != nil {
+		log.Fatal(error)
+	}
+	var user models.User
+	if error = json.Unmarshal(requestBody, &user); error != nil {
+		log.Fatal(error)
+	}
+
+	db, error := database.Connection()
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	repository := repositories.NewUserRepository(db)
+	userId, error := repository.Create(user)
+	if error != nil {
+		log.Fatal(error)
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Query executed sucessfully. Id inserted: %d", userId)))
 }
 
-//GetUsers fetches all users
+// GetUsers fetches all users
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Reading users"))
 }
 
-//GetUser fetches an user
+// GetUser fetches an user
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Reading user"))
 }
 
-//UpdateUser updates an user
+// UpdateUser updates an user
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Update user"))
 }
 
-//DeleteUser deletes an user
+// DeleteUser deletes an user
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Delete user"))
 }
---
+++ api/src/repositories/users.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+type users struct {
+	db *sql.DB
+}
+
+// NewUserRepository creates a user repository
+func NewUserRepository(db *sql.DB) *users {
+	return &users{db}
+}
+
+// Create inserts a new user in the database
+func (repository users) Create(user models.User) (uint32, error) {
+	statement, error := repository.db.Prepare(
+		"insert into users (name, nick, email, password) values (?,?,?,?)")
+	if error != nil {
+		return 0, error
+	}
+	defer statement.Close()
+	result, error := statement.Exec(user.Name, user.Nick, user.Email, user.Password)
+	if error != nil {
+		return 0, nil
+	}
+
+	lastInsertId, error := result.LastInsertId()
+	if error != nil {
+		return 0, error
+	}
+	defer repository.db.Close()
+	return uint32(lastInsertId), nil
+}
