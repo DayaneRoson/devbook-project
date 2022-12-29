cd120603f4b369f691a01908fecefa5f14b18f6d
Dayane Roson <dayane.roson@gmail.com>
2022-12-29 15:53:17 -0300

    jwt authentication

--- api/go.mod
+++ api/go.mod
@@ -7,6 +7,7 @@ require github.com/gorilla/mux v1.8.0
 require (
 	github.com/badoux/checkmail v1.2.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 )
---
+++ api/src/authentication/token.go
@@ -0,0 +1,17 @@
+package authentication
+
+import (
+	"time"
+
+	jwt "github.com/golang-jwt/jwt/v4"
+)
+
+// CreateToken returns a signed token with user permissions
+func CreateToken(userId uint64) (string, error) {
+	permissions := jwt.MapClaims{}
+	permissions["authorized"] = true
+	permissions["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	permissions["userId"] = userId
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
+	return token.SignedString([]byte("Secret"))
+}
