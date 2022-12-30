97fe0acff0ac28abf583bfbc492da0b081282a58
Dayane Roson <dayane.roson@gmail.com>
2022-12-30 14:55:03 -0300

    token validation

--- api/src/authentication/token.go
+++ api/src/authentication/token.go
@@ -2,6 +2,10 @@ package authentication
 
 import (
 	"api/src/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/golang-jwt/jwt/v4"
@@ -15,4 +19,32 @@ func CreateToken(userId uint64) (string, error) {
 	permissions["userId"] = userId
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
 	return token.SignedString([]byte(config.SecretKey))
+}
+
+// ValidateToken validates a token received
+func ValidateToken(r *http.Request) error {
+	stringToken := extractToken(r)
+	token, error := jwt.Parse(stringToken, returnKey)
+	if error != nil {
+		return error
+	}
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+	return errors.New("Invalid Token")
+}
+
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+	return ""
+}
+
+func returnKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected Signing Method %v", token.Header["alg"])
+	}
+	return config.SecretKey, nil
 }
